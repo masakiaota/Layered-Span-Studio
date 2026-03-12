c0a664b8d3dd0746f9c213cbbdcb8c61b6569ad5
Masaki AOTA <aotamasakimail@gmail.com>
2026-03-12 13:50:54 +0900

    build(frontend): vendor chunkを分割して警告を解消

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,24 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "scheduler"],
+          "router-vendor": ["react-router", "react-router-dom"],
+          "mui-vendor": [
+            "@emotion/react",
+            "@emotion/styled",
+            "@mui/icons-material",
+            "@mui/material",
+            "@popperjs/core",
+            "react-transition-group",
+          ],
+        },
+      },
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
